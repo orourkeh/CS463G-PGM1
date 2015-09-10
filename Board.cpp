b96db34eedc4bc8a134557fe7f4cf50c2a06bd0c
orourkeh <hdor222@uky.edu>
2015-09-10 18:48:03 -0400

    Refined Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -3,48 +3,90 @@
 #include <algorithm>
 #include <vector>
 #include <time.h>
+const int NUM_BALLS =20;
 class Board
 {
 private:
-	const int NUM_BALLS = 20;
 	std::vector<int> m_balls;
-	int numForwards = 0;
-	int numBackwards = 0;
-	int numRotate = 0;
+	Board* forwardChild;
+	Board* backwardChild;
+	Board* rotateChild;
+	int m_predicted;
+	int m_hueristic = 0;
 	void swap(int a, int b)
 	{
 		int tmp = m_balls[a];
 		m_balls[a] = m_balls[b];
 		m_balls[b] = tmp;
 	}
+	//end extra variables
 public:
 
 	//get balls
-	Board()
+	Board(int cost)
 	{
 		srand(time(NULL));
 		for (int i = 1; i <= NUM_BALLS; i++)//fill ball vector with 1 - 20
 		{
 			m_balls.push_back(i);
 		}
-		this->shuffle(100);
+		m_predicted = cost;//cost should be = to the depth of this node
+		this->shuffle(m_predicted);
+	}
+	int getHueristic()
+	{
+		//code to find value of huerstic
+		for(int i = 0; i < m_balls.size(); i++)
+		{
+			int leftNeighbor;
+			int rightNeighbor;
+			if(m_balls[i] == 1)
+			{
+				leftNeighbor = 20;
+				rightNeighbor = 2;
+			}
+			else if(m_balls[i] == 20)
+			{
+				leftNeighbor = 19;
+				rightNeighbor = 1;
+			}
+			else
+			{
+				leftNeighbor = m_balls[i] - 1;
+        	                rightNeighbor = m_balls[i] + 1;
+			}
+			m_hueristic += *(std::find(m_balls.begin(), m_balls.end(), leftNeighbor));// find returns iterator for integer
+			m_hueristic += *(std::find(m_balls,being(),
+		}
+		
+		return m_hueristic;
 	}
 	void moveForward()
 	{
 		std::rotate(m_balls.begin(), m_balls.end()-1, m_balls.end());
-		numForwards++; 
 	}
 	void moveBackwards()
 	{
 		std::rotate(m_balls.begin(), m_balls.begin() + 1, m_balls.end());
-		numBackwards++;
 	}
 	void rotate()
 	{
 		this->swap(12, 15);
 		this->swap(13, 14);
-		numRotate++;
 	}
+	Board* getForwardChild()
+        {
+                return forwardChild;
+        }
+        Board* getBackwardChild()
+        {
+                return backwardChild;
+        }
+        Board* getRotateChild()
+        {
+                return rotateChild;
+        }
+
 	void shuffle(int numMoves)
 	{
 		int count = 0;
@@ -62,7 +104,7 @@ public:
 						count++;
 					}
 				case 1:
-					if(lastMove != 0)
+					if(lastMove != 0)//stops undoing moves
 					{
 						this->moveBackwards();
 						count++;
@@ -70,12 +112,10 @@ public:
 				case 2:
 						this->rotate();
 						count++;
+						//will be undone if repeated 20 but that is an unlikly case
 			}
 		}
 		//reset stats for a new board
-		numForwards = 0;
-		numBackwards = 0;
-		numRotate = 0;
 	}
 	void display()
 	{
--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <vector>
 #include "Board.cpp"
-void play();
 int main()
 {
-	Board myBoard;
-	myBoard.display();
+	Board myBoard(100);//argument is the number of moves in shuffle
+	myBoard.display(); 
+	std::cout << myBoard.getHueristic() << std::endl;
 
-	return 0;
+
+	//create tree of boards with children containing all possible moves from parent
+	//Nodes will have 3 children forward, backward and rotate
+	//Moves that would undo last move will be set to NULL
+	
+	//To create child copy current Board then ->moveForward
+	//Mkae copy constructor jackass.
+return 0;
 }
